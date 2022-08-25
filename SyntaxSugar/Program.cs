d886381a8159e6e40dd82cce507d782de6a939b8
Katelyn-Collier <katelyncollier14@gmail.com>
2022-08-25 16:39:36 -0500

    Add project files.

---
+++ SyntaxSugar/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SyntaxSugar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" :
+            $"{answer} is greater than or equak to nine"; 
+            Console.WriteLine(response);
+        }
+    }
+}
